b42326aee714a0c1c6c8fed873e2751ab2f4f8a5
jlessuk <jonahdaxlessuk@gmail.com>
2023-09-14 16:46:42 -0500

    added volume file

---
+++ voloume_of_sphere.py
@@ -0,0 +1,9 @@
+def calculate_volume_of_sphere(radius)
+	pi = 3.14
+	volume = 4 * pi * radius**2
+	return volume
+
+r1 = 30
+r2 = 40 
+print(calculate_volume_of_sphere(r1))
+print(calculate_volume_of_sphere(r2))
